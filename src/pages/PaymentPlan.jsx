ded99080568367261111262c8e31fb29991f30d9
unknown <jagan@elvirainfotech.com>
2023-11-22 14:38:33 +0000

    payment

--- src/pages/PaymentPlan.jsx
+++ src/pages/PaymentPlan.jsx
@@ -19,7 +19,7 @@ const PaymentPlan = () => {
     return (
         <>
             {/* ======== subscription package  ======== */}
-            {location.state.subscription_pay && <>
+            {location.state?.subscription_pay && <>
 
                 <div>
                     <p>Choose Subscription Package</p>
@@ -35,7 +35,7 @@ const PaymentPlan = () => {
             </>}
 
             {/* ========= adduser pacakage ========= */}
-            {!location.state.subscription_pay && <>
+            {!location.state?.subscription_pay && <>
 
                 <div>
                     <p>Choose Payment Package</p>
@@ -52,7 +52,7 @@ const PaymentPlan = () => {
 
             <br />
 
-            <button type='button' className='btn btn-primary' onClick={navigate('/payment', { state: { subscription_pay: location.state.subscription_pay } })}>Proceed</button>
+            <button type='button' className='btn btn-primary' onClick={navigate('/payment', { state: { subscription_pay: location.state?.subscription_pay } })}>Proceed</button>
 
         </>
     );
