cf2cb40bf3311d61464a03fcc652588fcf1841a5
unknown <jagan@elvirainfotech.com>
2023-11-28 10:17:52 +0000

    remove from login

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -30,7 +30,7 @@ function Footer() {
                 <Link to="/privacypolicy" className="pp" >Privacy Policy</Link>
               </li>
               <li>
-                <Link to="/" className="pp" >Payment</Link>
+                <Link onClick={e => navigate('/payment-plan', { state: { subscription_pay: true } })} className="pp" >Payment</Link>
               </li>
             </ul>
             {/* <ul className='bottom-links'>
